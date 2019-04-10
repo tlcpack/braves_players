5b7acbe1a1dc96f3af3ded78720ae12719fe5664
Taylor Cooke <taylorcooke@gmail.com>
2019-04-10 14:54:36 -0400

    creating pages

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Player, Comment
 
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Comment)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Player, Comment
 
 # Create your views here.
+def index(request):
+    """View function for home page of site"""
+
+    players = Players.objects.all()
+
+    context = {
+        'players' = players
+    }
+
+    return render(request, 'index.html', context=context)
